2f52f29e59a91f1f15ffb953fecee2653e522b6c
Mateen <mateendev3@gmail.com>
2022-05-26 13:04:21 +0500

    Learned new Appbar Properties

--- lib/appbar/appbar.dart
+++ lib/appbar/appbar.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 void main(List<String> args) => runApp(const AppBarApp());
 
@@ -12,10 +11,95 @@ class AppBarApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: const Text('AppBar'),
-          systemOverlayStyle: const SystemUiOverlayStyle(
-            statusBarBrightness: Brightness.light,
-            statusBarColor: Colors.amber,
-          ),
+
+          //* centerTitle
+          // centerTitle: true,
+
+          //* actions
+          //* actionsIconTheme
+          // actions: [
+          //   IconButton(onPressed: () {}, icon: const Icon(Icons.smoke_free)),
+          //   IconButton(onPressed: () {}, icon: const Icon(Icons.smoke_free)),
+          //   IconButton(onPressed: () {}, icon: const Icon(Icons.smoke_free)),
+          //   IconButton(onPressed: () {}, icon: const Icon(Icons.smoke_free)),
+          // ],
+
+          // actionsIconTheme: const IconThemeData(
+          //   color: Colors.white,
+          //   opacity: 0.2,
+          //   shadows: [
+          //     Shadow(
+          //       color: Colors.black,
+          //       blurRadius: 30,
+          //       offset: Offset(2, 2),
+          //     ),
+          //   ],
+          //   size: 32.0,
+          // ),
+
+          //* leading
+          // leading: const IconButton(onPressed: null, icon: Icon(Icons.abc)),
+
+          //* automaticallyImplyLeading
+          // automaticallyImplyLeading: false,
+
+          //* backgroundColor
+          // backgroundColor: Colors.lime,
+
+          //* backwardsCompatibility @deprecated
+          // backwardsCompatibility: ,
+
+          //* bottom
+          //* bottomOpacity
+          // bottomOpacity: 0.5,
+          // bottom: AppBar(
+          //   actions: [
+          //     IconButton(
+          //       onPressed: () {},
+          //       icon: const Icon(
+          //         Icons.home,
+          //         color: Colors.black,
+          //       ),
+          //     ),
+          //     IconButton(
+          //       onPressed: () {},
+          //       icon: const Icon(
+          //         Icons.percent,
+          //         color: Colors.black,
+          //       ),
+          //     ),
+          //     IconButton(
+          //       onPressed: () {},
+          //       icon: const Icon(
+          //         Icons.people,
+          //         color: Colors.black,
+          //       ),
+          //     ),
+          //     IconButton(
+          //       onPressed: () {},
+          //       icon: const Icon(
+          //         Icons.car_crash,
+          //         color: Colors.black,
+          //       ),
+          //     ),
+          //   ],
+          // ),
+
+          //* elevation.
+          // elevation: 8.0,
+
+          excludeHeaderSemantics: true,
+
+          //* brightness
+          //* systemOverlayStyle
+          // systemOverlayStyle: const SystemUiOverlayStyle(
+          //   statusBarBrightness: Brightness.light,
+          //   statusBarColor: Colors.green,
+          //   statusBarIconBrightness: Brightness.light,
+          //   systemNavigationBarColor: Colors.pink,
+          //   systemNavigationBarDividerColor: Colors.red,
+          //   systemNavigationBarIconBrightness: Brightness.light,
+          // ),
           //* shape
           // shape: RoundedRectangleBorder(
           //   side: const BorderSide(color: Colors.red, width: 5.0),
